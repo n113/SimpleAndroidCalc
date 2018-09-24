c46562f03dbdcf93aeb8b97b825dad21d59cabff
n113 <rfedsxc@mail.ru>
2018-09-24 11:42:45 -0700

    history added. GetResult is now part of Math class

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
-import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener{
     TextView resultTV;
@@ -65,6 +64,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 
 
     public void setResultTV(int operation){
+        Math math = new Math();
         TextView historyTV=findViewById(R.id.historyTV);
         String a=numA.getText().toString();
         String b=numB.getText().toString();
@@ -72,23 +72,22 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         String equals=" = ";
         String result=null;
 
-
         switch(operation){
             case 0 :
-                result=getResult(a,b,0);
+                result=math.getResult(a,b,0);
                 resultTV.setText(result);
                 operations=" + ";
                 break;
             case 1 :
-                result=getResult(a,b,1);
+                result=math.getResult(a,b,1);
                 resultTV.setText(result);
                 break;
             case 2 :
-                result=getResult(a,b,2);
+                result=math.getResult(a,b,2);
                 resultTV.setText(result);
                 break;
             case 3 :
-                result=getResult(a,b,3);
+                result=math.getResult(a,b,3);
                 resultTV.setText(result);
                 break;
         }
@@ -97,39 +96,14 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         historyTV.setText(history);
     }
 
-    public String getResult(String numA, String numB, int operation){
 
-        double a = stringTodouble(numA);
-        double b = stringTodouble(numB);
-
-        Math math = new Math();
-        String toReturn=null;
-        switch(operation){
-            case 0 :
-                toReturn= String.valueOf(math.sum(a,b));
-                break;
-            case 1 :
-                toReturn= String.valueOf(math.subs(a,b));
-                break;
-            case 2 :
-                toReturn= String.valueOf(math.multyp(a,b));
-                break;//
-            case 3 :
-                toReturn= String.valueOf(math.devide(a,b));
-                break;
-        }
-        return  toReturn;
-    }
 
 
     public void makeToast(String text) {
         Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 
-    public double stringTodouble(String string){
-        Scanner scanner = new Scanner(string);
-        return scanner.nextDouble();
-    }
+
 
 
 }//class
--- app/src/main/java/com/example/user/myapplication/Math.java
+++ app/src/main/java/com/example/user/myapplication/Math.java
@@ -1,19 +1,50 @@
 package com.example.user.myapplication;
 
+import java.util.Scanner;
+
 public class Math {
-    public double sum(double a, double b){
+    private double sum(double a, double b){
         return a+b;
     }
 
-    public double subs(double a, double b){
+    private double subs(double a, double b){
         return a-b;
     }
 
-    public double multyp(double a, double b){
+    private double multyp(double a, double b){
         return a*b;
     }
 
-    public double devide(double a, double b){
-        return a/b;////
+    private double devide(double a, double b){
+        return a/b;
+    }
+
+    public String getResult(String numA, String numB, int operation){
+
+        double a = stringTodouble(numA);
+        double b = stringTodouble(numB);
+
+        Math math = new Math();
+        String toReturn=null;
+        switch(operation){
+            case 0 :
+                toReturn= String.valueOf(math.sum(a,b));
+                break;
+            case 1 :
+                toReturn= String.valueOf(math.subs(a,b));
+                break;
+            case 2 :
+                toReturn= String.valueOf(math.multyp(a,b));
+                break;//
+            case 3 :
+                toReturn= String.valueOf(math.devide(a,b));
+                break;
+        }
+        return  toReturn;
+    }
+
+    public double stringTodouble(String string){
+        Scanner scanner = new Scanner(string);
+        return scanner.nextDouble();
     }
 }
