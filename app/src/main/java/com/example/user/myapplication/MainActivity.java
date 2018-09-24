8419f3dfe3e61ee651c6aab2f3a8e177b298309f
n113 <rfedsxc@mail.ru>
2018-09-24 10:45:57 -0700

    operatins's spinner was replaced with operations buttons

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -3,76 +3,73 @@ package com.example.user.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.view.View.OnClickListener;
 import java.util.Scanner;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
     TextView resultTV;
     EditText numA;
     EditText numB ;
-    Spinner operationsSpinner ;
-    int selected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        operationsSpinner = findViewById(R.id.operationsSpinner);
 
         numA = findViewById(R.id.numA);
         numB = findViewById(R.id.numB);
         resultTV = findViewById(R.id.resultTV);
-
         numA.setText("0");
         numB.setText("0");
 
-        // Create an ArrayAdapter using the string array and a default spinner layout (Pay attention to the the stationsName array in the Strings which contains spinner items. You can find its code below)
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.OperationsSpinnerStrings, android.R.layout.simple_spinner_item);
+        Button sumBtn = findViewById(R.id.sumBtn);
+        Button subsBtn = findViewById(R.id.subsBtn);
+        Button multyplBtn = findViewById(R.id.multyplBtn);
+        Button devideBtn = findViewById(R.id.devideBtn);
+        Button clearBtn = findViewById(R.id.clearBtn);
 
-        // Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // Apply the adapter to the spinner
-        operationsSpinner.setAdapter(adapter);
-        selected = operationsSpinner.getSelectedItemPosition();
-
-        operationsSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
-        {
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id)
-            {
-                selected=position;
-                setResultTV (selected);
-            } // to close the onItemSelected
-
-
-            public void onNothingSelected(AdapterView<?> parent)
-            {
-            }
-        });
-    }
-    public void onClear (View view){
-        numA.setText(null);
-        numB.setText(null);
-        resultTV.setText(null);
+        sumBtn.setOnClickListener(this);
+        subsBtn.setOnClickListener(this);
+        multyplBtn.setOnClickListener(this);
+        devideBtn.setOnClickListener(this);
+        clearBtn.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.sumBtn:
+                setResultTV(0);
+                break;
+            case R.id.subsBtn:
+                setResultTV(1);
+                break;
+            case R.id.multyplBtn:
+                setResultTV(2);
+                break;
+            case R.id.devideBtn:
+                setResultTV(3);
+                break;
+            case R.id.clearBtn:
+                numA.setText(null);
+                numB.setText(null);
+                resultTV.setText(null);
+                break;
+        }
+    }
 
-    public void setResultTV(int position){
 
-
+    public void setResultTV(int operation){
         double a = stringTodouble(numA.getText().toString());
         double b = stringTodouble(numB.getText().toString());
-
         Math math = new Math();
 
-        switch(position){
+        switch(operation){
             case 0 :
                 resultTV.setText(null);
                 resultTV.setText(String.valueOf(math.sum(a,b)));
@@ -100,4 +97,6 @@ public class MainActivity extends AppCompatActivity {
         Scanner scanner = new Scanner(string);
         return scanner.nextDouble();
     }
+
+
 }//class
